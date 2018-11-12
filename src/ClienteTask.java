4b685b4c60a4fa9a89c31f975cf7d4ea5caea0c5
boleointenso <36081822+boleointenso@users.noreply.github.com>
2018-11-12 17:55:13 -0500

    boleo

--- src/ClienteTask.java
+++ src/ClienteTask.java
@@ -2,27 +2,32 @@ import javax.security.cert.CertificateEncodingException;
 
 import uniandes.gload.core.Task;
 
-public class ClienteTask extends Task{
+public class ClienteTask extends Task
+{
 
 private static int fallas = 0; 
 	
 	@Override
-	public void fail() {
+	public void fail() 
+	{
 		System.out.println(Task.MENSAJE_FAIL);
 		setFallas(getFallas() + 1); 
 	}
 
 	@Override
-	public void success() {
+	public void success() 
+	{
 		System.out.println(Task.OK_MESSAGE);
 		
 	}
 
 	@Override
-	public void execute() {
-		
-			try {
-				try {
+	public void execute() 
+	{		
+			try 
+			{
+				try 
+				{
 					Cliente cliente = new Cliente(Cliente.SEGURIDAD);
 				}
 				 catch (Exception e)
@@ -30,20 +35,20 @@ private static int fallas = 0;
 
 					e.printStackTrace();
 				}
-				} catch (Exception e)
+			} 
+			catch (Exception e)
 			{
 					e.printStackTrace();
-			}
-		
+			}		
 	}
 
-	public static int getFallas() {
+	public static int getFallas() 
+	{
 		return fallas;
 	}
 
-	public synchronized static void setFallas(int fallas) {
+	public synchronized static void setFallas(int fallas) 
+	{
 		ClienteTask.fallas = fallas;
-	}
-	
-	
+	}	
 }
--- src/Monitor.java
+++ src/Monitor.java
@@ -6,23 +6,16 @@ import javax.management.AttributeList;
 import javax.management.MBeanServer;
 import javax.management.ObjectName;
 
-public class Monitor extends Thread{
-
-	
-	private long startVer; 
-	private long startConsu; 
-	
-	
+public class Monitor extends Thread
+{
 	private static ArrayList<Long> tiemposVerificacion = new ArrayList<>();
 	private static ArrayList<Long> tiemposConsulta = new ArrayList<>();
 	
-	private double memoria;
-	
+	private long startVer; 
+	private long startConsu;
+	private double memoria;	
 	private boolean terminado;
-
 	private String caso;
-	
-	
 	
 	public Monitor(String pCaso)
 	{
@@ -30,50 +23,40 @@ public class Monitor extends Thread{
 		terminado = false; 
 	}
 	
-	public void termino(String pCaso) {
+	public void termino(String pCaso)
+	{
 		terminado = true; 
-		caso= pCaso;
-		
+		caso= pCaso;		
 	}
 	
 	public void comenzar()
 	{
 		startVer = System.currentTimeMillis(); 
 		System.out.println("Tiempo start " + startVer);
-	}
-	
+	}	
 	
 	public long endVer()
-	{	
-		
+	{			
 		long fin = System.currentTimeMillis(); 
-		long resta = fin-startVer; 
-		
-		System.out.println("Tiempo fin " + fin );
-		
+		long resta = fin-startVer; 		
+		System.out.println("Tiempo fin " + fin );		
 		System.out.println("Tiempo resta ver "+ resta);
-		synchronized (this) {
-			
-			
-				tiemposVerificacion.add(resta);
-			
-		}
-		
+		synchronized (this) 
+		{					
+			tiemposVerificacion.add(resta);			
+		}		
 		return fin - startVer; 
 	}
 	
 	public long endConsu()
-	{	
-		
+	{			
 		long fin = System.currentTimeMillis(); 
-
-		long resta = fin-startVer; 
-		
+		long resta = fin-startVer; 		
 		System.out.println("Tiempo fin consul "+ resta);
-		synchronized (this) {
+		synchronized (this) 
+		{
 				tiemposConsulta.add(resta);
-		}
-		
+		}		
 		return fin - startVer; 
 	}
 	
@@ -89,9 +72,8 @@ public class Monitor extends Thread{
 		tiemposVerificacion.add(ver);
 	}
 	
-	public double getSystemCpuLoad() throws Exception {
-		
-		
+	public double getSystemCpuLoad() throws Exception 
+	{				
 		 MBeanServer mbs = ManagementFactory.getPlatformMBeanServer();
 		 ObjectName name = ObjectName.getInstance("java.lang:type=OperatingSystem");
 		 AttributeList list = mbs.getAttributes(name, new String[]{ "SystemCpuLoad" });
@@ -102,26 +84,19 @@ public class Monitor extends Thread{
 		 if (value == -1.0) return Double.NaN;
 		 // returns a percentage value with 1 decimal point precision
 		 return ((int)(value * 1000) / 10.0);
-		 }
-	
-	
+	}
 	
 	public static double getTiemposDeVerificacionPromedio()
 	{
-		double suma = 0.0; 
-		
-		for (Long long1 : tiemposVerificacion) {
+		double suma = 0.0; 		
+		for (Long long1 : tiemposVerificacion) 
+		{
 			suma += (double)long1;
-		}
-		
-		
+		}	
 		System.out.println("Suma = " + suma);
-		System.out.println("Tamaño = " + tiemposVerificacion.size());
-		
-		double tamanio = (double)tiemposVerificacion.size();
-		
-		return suma/tamanio ; 
-		
+		System.out.println("Tamaño = " + tiemposVerificacion.size());		
+		double tamanio = (double)tiemposVerificacion.size();		
+		return suma/tamanio ; 		
 	}
 	
 	public synchronized static void addTiemposVerificacion(long valor)
@@ -132,13 +107,13 @@ public class Monitor extends Thread{
 	public synchronized static void addTiemposConsulta(long valor)
 	{
 		tiemposConsulta.add(valor); 
-	}
-	
+	}	
 	
 	public static ArrayList<Long> getTiemposVerificacion()
 	{
 		return tiemposVerificacion; 
 	}
+	
 	public static ArrayList<Long> getTiemposConsulta()
 	{
 		return tiemposConsulta; 
@@ -156,49 +131,48 @@ public class Monitor extends Thread{
 	
 	public static double getTiemposDeConsultaPromedio()
 	{
-		double suma = 0L; 
-		
-		for (Long long1 : tiemposConsulta) {
+		double suma = 0L; 		
+		for (Long long1 : tiemposConsulta) 
+		{
 			suma += long1;
-		}
-		
-		return suma/(double)tiemposConsulta.size(); 
-		
+		}		
+		return suma/(double)tiemposConsulta.size(); 		
 	}
 	
 	public boolean getTermino()
 	{
 		return terminado; 
 	}
+	
 	@Override
 	public void run()
 	{
 		long start = System.currentTimeMillis(); 
 		System.out.println("start " + caso +  "= " + start);
-		try {
-			synchronized (this) {
-				
+		try 
+		{
+			synchronized (this) 
+			{				
 				wait();
 				System.out.println(caso + " after wait");
-			}
-			
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			}			
+		} 
+		catch (InterruptedException e) 
+		{
 			e.printStackTrace();
 		} 
 		long fin = System.currentTimeMillis() - 10;
 		System.out.println("fin " + caso + "= " + fin );
 		long resta = fin-start;
-		System.out.println("RESTA "+ caso + "= " + resta );
-		
+		System.out.println("RESTA "+ caso + "= " + resta );		
 		if(caso.equals("verificacion"))
 		{
 			addVer(resta);
-		}else if(caso.equals("consulta")) {
-			addConsulta(resta);
 		}
-		
+		else if(caso.equals("consulta")) 
+		{
+			addConsulta(resta);
+		}		
 		terminado = true; 
-	}
-	
+	}	
 }
